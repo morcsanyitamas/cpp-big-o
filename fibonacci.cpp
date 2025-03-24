5ddb45795fe6bbf4cafa4a5835e18ea49acdfc47
Tamás Morcsányi <mtomi8@gmail.com>
2025-03-24 11:26:03 +0100

    add fibonacci cpp

---
+++ fibonacci.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+// iterative fibonacci function
+int fibonacci_iterative(int const n) {
+}
+
+// recursive fibonacci function
+int fibonacci(int const n) {
+}
+
+// Recursive Approach with Memoization
+int fibonacci_memoization(int const n, int *memo) {
+}
+
+// Tail Recursive Approach
+int fibonacci_tail_recursive(int const n, int const a = 0, int const b = 1) {
+}
+
+int main() {
+    int n = 10;
+    int result = fibonacci(n);
+    std::cout << "Fibonacci of " << n << " is " << result << std::endl;
+    return 0;
+}
